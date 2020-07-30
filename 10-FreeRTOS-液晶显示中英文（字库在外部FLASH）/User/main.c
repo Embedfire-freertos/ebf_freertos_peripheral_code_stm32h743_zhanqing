8c46dea99286b6a535fad383a71e53814fae8a67
hejiawen <hjw0415@outlook.com>
2020-07-30 11:49:09 +0800

    增加液晶显示中英文例程

---
+++ 10-FreeRTOS-液晶显示中英文（字库在外部FLASH）/User/main.c
@@ -0,0 +1,425 @@
+/**
+  *********************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2018-xx-xx
+  * @brief   FreeRTOS V9.0.0  + STM32 中断管理
+  *********************************************************************
+  * @attention
+  *
+  * 实验平台:野火  STM32 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  **********************************************************************
+  */ 
+ 
+/*
+*************************************************************************
+*                             包含的头文件
+*************************************************************************
+*/ 
+/* 开发板硬件bsp头文件 */
+#include "stm32h7xx.h"
+#include "main.h"
+#include "bsp_led.h"
+#include "bsp_key.h" 
+#include "core_delay.h"   
+#include "bsp_debug_usart.h"
+#include "./flash/bsp_spi_flash.h"
+#include "./lcd/bsp_ili9806g_lcd.h"
+/* FreeRTOS头文件 */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+/* 标准库头文件 */
+#include <string.h>
+/* FatFs文件系统 */
+#include "fatfs_flash_qspi.h"
+#include "ff_gen_drv.h"
+#include "ff.h"
+
+/**************************** 任务句柄 ********************************/
+/* 
+ * 任务句柄是一个指针，用于指向一个任务，当任务创建好之后，它就具有了一个任务句柄
+ * 以后我们要想操作这个任务都需要通过这个任务句柄，如果是自身的任务操作自己，那么
+ * 这个句柄可以为NULL。
+ */
+ /* 创建任务句柄 */
+static TaskHandle_t AppTaskCreate_Handle;
+/* LED任务句柄 */
+static TaskHandle_t Test_Task_Handle = NULL;
+/* KEY任务句柄 */
+static TaskHandle_t KEY_Task_Handle = NULL;
+/********************************** 内核对象句柄 *********************************/
+/*
+ * 信号量，消息队列，事件标志组，软件定时器这些都属于内核的对象，要想使用这些内核
+ * 对象，必须先创建，创建成功之后会返回一个相应的句柄。实际上就是一个指针，后续我
+ * 们就可以通过这个句柄操作这些内核对象。
+ *
+ * 内核对象说白了就是一种全局的数据结构，通过这些数据结构我们可以实现任务间的通信，
+ * 任务间的事件同步等各种功能。至于这些功能的实现我们是通过调用这些内核对象的函数
+ * 来完成的
+ * 
+ */
+
+
+/******************************* 全局变量声明 ************************************/
+/*
+ * 当我们在写应用程序的时候，可能需要用到一些全局变量。
+ */
+char SPIPath[4]; /* SPI flash逻辑驱动器路径 */
+FATFS fs;													/* FatFs文件系统对象 */
+FIL fnew;													/* 文件对象 */
+FRESULT res_flash;                /* 文件操作结果 */
+UINT fnum;            					  /* 文件成功读写数量 */
+BYTE ReadBuffer[1024]={0};        /* 读缓冲区 */
+BYTE WriteBuffer[] =              /* 写缓冲区*/
+"欢迎使用野火STM32 H743开发板 今天是个好日子，新建文件系统测试文件\r\n";  
+extern Diskio_drvTypeDef  SPI_Driver;
+/*
+*************************************************************************
+*                             函数声明
+*************************************************************************
+*/
+static void AppTaskCreate(void);/* 用于创建任务 */
+
+static void Test_Task(void* pvParameters);/* Test_Task任务实现 */
+static void KEY_Task(void* pvParameters);/* KEY_Task任务实现 */
+
+static void BSP_Init(void);/* 用于初始化板载相关资源 */
+static void LCD_Test(void);
+/*****************************************************************
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  * @note   第一步：开发板硬件初始化 
+            第二步：创建APP应用任务
+            第三步：启动FreeRTOS，开始多任务调度
+  ****************************************************************/
+int main(void)
+{	
+  BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
+  /* 开发板硬件初始化 */
+  BSP_Init();
+	
+  printf("这是一个[野火]-STM32全系列开发板-FreeRTOS固件库例程！\n\n");
+  printf("按下KEY1挂起任务，按下KEY2恢复任务，LCD现实\n");
+	
+   /* 创建 AppTaskCreate 任务 */
+  xReturn = xTaskCreate((TaskFunction_t )AppTaskCreate,  /* 任务入口函数 */
+                        (const char*    )"AppTaskCreate",/* 任务名字 */
+                        (uint16_t       )512,  /* 任务栈大小 */
+                        (void*          )NULL,/* 任务入口函数参数 */
+                        (UBaseType_t    )1, /* 任务的优先级 */
+                        (TaskHandle_t*  )&AppTaskCreate_Handle);/* 任务控制块指针 */ 
+															
+	if(pdFAIL != xReturn)/* 创建成功 */
+    vTaskStartScheduler();   /* 启动任务，开启调度 */
+  
+  while(1);   /* 正常不会执行到这里 */    
+}
+
+
+/***********************************************************************
+  * @ 函数名  ： AppTaskCreate
+  * @ 功能说明： 为了方便管理，所有的任务创建函数都放在这个函数里面
+  * @ 参数    ： 无  
+  * @ 返回值  ： 无
+  **********************************************************************/
+static void AppTaskCreate(void)
+{
+  BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
+  
+  taskENTER_CRITICAL();           //进入临界区
+  
+  /* 创建Test_Task任务 */
+  xReturn = xTaskCreate((TaskFunction_t )Test_Task, /* 任务入口函数 */
+                        (const char*    )"Test_Task",/* 任务名字 */
+                        (uint16_t       )1024,   /* 任务栈大小 */
+                        (void*          )NULL,	/* 任务入口函数参数 */
+                        (UBaseType_t    )2,	    /* 任务的优先级 */
+                        (TaskHandle_t*  )&Test_Task_Handle);/* 任务控制块指针 */
+  if(pdPASS == xReturn)
+    printf("创建Test_Task任务成功!\r\n");
+  /* 创建KEY_Task任务 */
+  xReturn = xTaskCreate((TaskFunction_t )KEY_Task,  /* 任务入口函数 */
+                        (const char*    )"KEY_Task",/* 任务名字 */
+                        (uint16_t       )512,  /* 任务栈大小 */
+                        (void*          )NULL,/* 任务入口函数参数 */
+                        (UBaseType_t    )3, /* 任务的优先级 */
+                        (TaskHandle_t*  )&KEY_Task_Handle);/* 任务控制块指针 */ 
+  if(pdPASS == xReturn)
+    printf("创建KEY_Task任务成功!\r\n");
+  
+  vTaskDelete(AppTaskCreate_Handle); //删除AppTaskCreate任务
+  
+  taskEXIT_CRITICAL();            //退出临界区
+}
+
+
+/**********************************************************************
+  * @ 函数名  ： Test_Task
+  * @ 功能说明： Test_Task任务主体
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  ********************************************************************/
+static void Test_Task(void* parameter)
+{
+  while (1)
+  {
+		LCD_Test();
+    vTaskDelay(1000);   /* 延时1000个tick */
+  }
+}
+
+/**********************************************************************
+  * @ 函数名  ： Test_Task
+  * @ 功能说明： Test_Task任务主体
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  ********************************************************************/
+static void KEY_Task(void* parameter)
+{	
+  while (1)
+  {
+    if( Key_Scan(KEY1_GPIO_PORT,KEY1_PIN) == KEY_ON )
+    {/* K1 被按下 */
+      printf("挂起Test任务！\n");
+      vTaskSuspend(Test_Task_Handle);/* 挂起LED任务 */
+      printf("挂起Test任务成功！\n");
+    } 
+    if( Key_Scan(KEY2_GPIO_PORT,KEY2_PIN) == KEY_ON )
+    {/* K2 被按下 */
+      printf("恢复Test任务！\n");
+      vTaskResume(Test_Task_Handle);/* 恢复LED任务！ */
+      printf("恢复Test任务成功！\n");
+    }
+    vTaskDelay(20);/* 延时20个tick */
+  }
+}
+/***********************************************************************
+  * @ 函数名  ： BSP_Init
+  * @ 功能说明： 板级外设初始化，所有板子上的初始化均可放在这个函数里面
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  *********************************************************************/
+static void BSP_Init(void)
+{
+	/*
+	 * STM32中断优先级分组为4，即4bit都用来表示抢占优先级，范围为：0~15
+	 * 优先级分组只需要分组一次即可，以后如果有其他的任务需要用到中断，
+	 * 都统一用这个优先级分组，千万不要再分组，切忌。
+	 */
+	/* 初始化系统时钟 */
+	SystemClock_Config();
+	
+	/* 配置优先级分组为4 */
+	HAL_NVIC_SetPriorityGrouping( NVIC_PRIORITYGROUP_4 );
+	
+	/* LED 初始化 */
+	LED_GPIO_Config();
+	
+	/* KEY 初始化 */
+	Key_GPIO_Config();
+	
+	/* 串口初始化	*/
+	DEBUG_USART_Config();
+	
+  /* 16M串行flash W25Q128初始化 */
+	SPI_FLASH_Init();
+	
+	/* LCD 初始化 */
+	ILI9806G_Init();    
+}
+
+/*用于测试各种液晶的函数*/
+void LCD_Test(void)
+{
+	printf("\r\n ********** 液晶屏中文显示程序（字库在外部FLASH）*********** \r\n"); 
+	printf("\r\n 若汉字显示不正常，请阅读工程中的readme.txt文件说明，根据要求给FLASH重刷字模数据\r\n"); 
+	//其中0、3、5、6 模式适合从左至右显示文字，
+	//不推荐使用其它模式显示文字	其它模式显示文字会有镜像效果			
+	//其中 6 模式为大部分液晶例程的默认显示方向  
+	ILI9806G_GramScan ( 6 );
+	/*演示显示变量*/
+	static uint8_t testCNT = 0;	
+	char dispBuff[100];
+	
+	testCNT++;	
+	
+	LCD_SetFont(&Font16x32);
+	LCD_SetColors(RED,BLACK);
+
+  ILI9806G_Clear(0,0,LCD_X_LENGTH,LCD_Y_LENGTH);	/* 清屏，显示全黑 */
+	/********显示字符串示例*******/ 
+  ILI9806G_DispStringLine_EN_CH(LINE(0),"野火4.5寸LCD参数：");
+  ILI9806G_DispStringLine_EN_CH(LINE(2),"分辨率：480x854 px");
+  ILI9806G_DispStringLine_EN_CH(LINE(3),"ILI9806G液晶驱动");
+  ILI9806G_DispStringLine_EN_CH(LINE(4),"GT5688触摸屏驱动");
+
+	/********显示变量示例*******/
+	LCD_SetTextColor(GREEN);
+
+	/*使用c标准库把变量转化成字符串*/
+	sprintf(dispBuff,"显示变量计数 : %d ",testCNT);
+  ILI9806G_ClearLine(LINE(7));	/* 清除单行文字 */
+	
+	/*然后显示该字符串即可，其它变量也是这样处理*/
+	ILI9806G_DispStringLine_EN(LINE(7),dispBuff);
+
+	/*******显示图形示例******/
+  /* 画直线 */
+  
+  ILI9806G_ClearLine(LINE(7));/* 清除单行文字 */
+	LCD_SetTextColor(BLUE);
+
+  ILI9806G_DispStringLine_EN_CH(LINE(7),"画直线:");
+  
+	LCD_SetTextColor(RED);
+  ILI9806G_DrawLine(50,270,420,275);  
+  ILI9806G_DrawLine(50,300,420,375);
+  
+	LCD_SetTextColor(GREEN);
+  ILI9806G_DrawLine(50,370,420,475);  
+  ILI9806G_DrawLine(50,400,420,475);
+	
+	LCD_SetTextColor(BLUE);
+  ILI9806G_DrawLine(50,420,420,325);  
+  ILI9806G_DrawLine(50,450,420,395);
+  
+  vTaskDelay(1000);
+  
+  ILI9806G_Clear(0,32*7,LCD_X_LENGTH,LCD_Y_LENGTH-32*7);	/* 清屏，显示全黑 */
+  
+  
+  /*画矩形*/
+
+  ILI9806G_ClearLine(LINE(7));	/* 清除单行文字 */
+	LCD_SetTextColor(BLUE);
+
+  ILI9806G_DispStringLine_EN_CH(LINE(7),"画矩形:");
+
+	LCD_SetTextColor(RED);
+  ILI9806G_DrawRectangle(50,300,200,100,1);
+	
+	LCD_SetTextColor(GREEN);
+  ILI9806G_DrawRectangle(100,300,200,120,0);
+	
+	LCD_SetTextColor(BLUE);
+  ILI9806G_DrawRectangle(250,300,200,150,1);
+  
+  vTaskDelay(1000);
+	
+	ILI9806G_Clear(0,32*7,LCD_X_LENGTH,LCD_Y_LENGTH-32*7);	/* 清屏，显示全黑 */
+
+  /* 画圆 */
+  ILI9806G_ClearLine(LINE(7));	/* 清除单行文字 */
+	LCD_SetTextColor(BLUE);
+	
+  ILI9806G_DispStringLine_EN_CH(LINE(7),"画圆:");
+
+	LCD_SetTextColor(RED);
+	ILI9806G_DrawCircle(150,400,60,1);
+
+	LCD_SetTextColor(GREEN);
+	ILI9806G_DrawCircle(250,400,60,0);
+
+	LCD_SetTextColor(BLUE);
+	ILI9806G_DrawCircle(350,400,60,1);
+
+  vTaskDelay(1000);
+  
+  ILI9806G_Clear(0,32*7,LCD_X_LENGTH,LCD_Y_LENGTH-32*7);	/* 清屏，显示全黑 */
+
+}
+
+
+/**
+  * @brief  System Clock 配置
+  *         system Clock 配置如下: 
+	*            System Clock source  = PLL (HSE)
+	*            SYSCLK(Hz)           = 480000000 (CPU Clock)
+	*            HCLK(Hz)             = 240000000 (AXI and AHBs Clock)
+	*            AHB Prescaler        = 2
+	*            D1 APB3 Prescaler    = 2 (APB3 Clock  120MHz)
+	*            D2 APB1 Prescaler    = 2 (APB1 Clock  120MHz)
+	*            D2 APB2 Prescaler    = 2 (APB2 Clock  120MHz)
+	*            D3 APB4 Prescaler    = 2 (APB4 Clock  120MHz)
+	*            HSE Frequency(Hz)    = 25000000
+	*            PLL_M                = 5
+	*            PLL_N                = 192
+	*            PLL_P                = 2
+	*            PLL_Q                = 4
+	*            PLL_R                = 2
+	*            VDD(V)               = 3.3
+	*            Flash Latency(WS)    = 4
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+  HAL_StatusTypeDef ret = HAL_OK;
+  
+  /*使能供电配置更新 */
+  MODIFY_REG(PWR->CR3, PWR_CR3_SCUEN, 0);
+
+  /* 当器件的时钟频率低于最大系统频率时，电压调节可以优化功耗，
+		 关于系统频率的电压调节值的更新可以参考产品数据手册。  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+ 
+  /* 启用HSE振荡器并使用HSE作为源激活PLL */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+  RCC_OscInitStruct.CSIState = RCC_CSI_OFF;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+
+  RCC_OscInitStruct.PLL.PLLM = 5;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 24;
+ 
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+  ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+  
+	/* 选择PLL作为系统时钟源并配置总线时钟分频器 */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK  | \
+																 RCC_CLOCKTYPE_HCLK    | \
+																 RCC_CLOCKTYPE_D1PCLK1 | \
+																 RCC_CLOCKTYPE_PCLK1   | \
+                                 RCC_CLOCKTYPE_PCLK2   | \
+																 RCC_CLOCKTYPE_D3PCLK1);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;  
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2; 
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2; 
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2; 
+  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SPI1;
+  PeriphClkInitStruct.Spi123ClockSelection = RCC_SPI123CLKSOURCE_PLL;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    while(1) { ; }
+  }
+}
+/****************************END OF FILE***************************/
